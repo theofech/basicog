949114b173a7e59c39a044227b243365f5004cb4
Theodore <theodore@lebasic.com>
2021-01-08 12:54:34 +0100

    test

--- import_modele_companies.py
+++ import_modele_companies.py
@@ -2,15 +2,16 @@ listCompanies=["Mercedes","Volswagen","Bayer AG","Adidas"]
 listYears=["2015","2016","2017"]
 listIndicators=["Turnover","Net profit Whole of group","Ordinary dividend"]
 
-strChosenCompanie=listCompanies[0]
-strChosenYear=listYear[0]
-strChosenIndicator=listIndicator[0]
+strChosenCompany=listCompanies[0]
+strChosenYear=listYears[0]
+strChosenIndicator=listIndicators[0]
 
 def change_list_element(list,element,increment):
 	if element in list:
-  	return list[list.index(element)+increment]
+		print(list[list.index(element)+increment])
+		return list[list.index(element)+increment]
 	else:
-		print("Fin de la liste")
+		print("Element non dans la liste")
 		exit
-		
-  
+
+
