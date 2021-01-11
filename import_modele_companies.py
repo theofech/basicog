7509596f747d7a98fbd00c996bf07a6da1258513
Theodore <theodore@lebasic.com>
2021-01-11 17:40:29 +0100

    Ajout de l'entrée du username et du mot de passe en ligne de commande

--- import_modele_companies.py
+++ import_modele_companies.py
@@ -1,3 +1,16 @@
+import sys, getopt #https://www.tutorialspoint.com/python/python_command_line_arguments.htm
+
+print(sys.argv)
+strUsername= sys.argv[1]
+strPassword= sys.argv[2]
+
+
+print("UserName : "+strUsername)
+print("Password : "+strPassword)
+
+
+
+
 listCompanies=["Mercedes","Volswagen","Bayer AG","Adidas"]
 listYears=["2015","2016","2017"]
 listIndicators=["Turnover","Net profit Whole of group","Ordinary dividend"]
@@ -15,4 +28,5 @@ def change_list_element(list,element,increment):
 		exit
 
 def add_raw_data(strCompany,strYear,strIndicator,floatRawData,intPage,strDocument,strComment):
+	print(strComment)
 
